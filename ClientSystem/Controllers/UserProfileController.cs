5cbb4cd0a54d6dbbd7d9cda86320ff3ca477ae39
sifat-hossain <43630944+sifat-hossain@users.noreply.github.com>
2019-11-06 02:11:46 +0600

    Create layout

---
+++ ClientSystem/Controllers/UserProfileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClientSystem.Models;
+
+namespace ClientSystem.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private ClientSystemEntities db = new ClientSystemEntities();
+        // GET: UserProfile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
